82702627881ee9332d24bc7fce7bd6f93c96363f
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-01-14 19:06:54 +0000

    Practice creating a react frontend that fetches from an express api

---
+++ practice-react-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import APITest from './APITest/APITest';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the React Frontend!</h1>
+      <p>Below is an API test:</p>
+      <APITest />
+    </div>
+  );
+}
+
+export default App;
